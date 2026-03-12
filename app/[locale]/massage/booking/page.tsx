5b08abfec9d96b74e7847b41dffa45b53d0ae3e6
EuvhenRight <ugnivenko.ea@gmail.com>
2026-03-12 21:48:10 +0100

    fix scrolling again

--- app/[locale]/massage/booking/page.tsx
+++ app/[locale]/massage/booking/page.tsx
@@ -37,9 +37,9 @@ export default function MassageBookingPage() {
   }));
 
   return (
-    <main className="h-[100dvh] max-h-[100dvh] min-h-0 bg-nearBlack text-icyWhite flex flex-col overflow-hidden overscroll-none">
+    <main className="fixed inset-0 z-0 h-[100dvh] max-h-[100dvh] bg-nearBlack text-icyWhite flex flex-col overflow-hidden overscroll-none">
       <Navbar />
-      <div className="flex-1 flex flex-col min-h-0 pt-16 md:pt-20 pb-4 md:pb-6 px-4 sm:px-6 lg:px-8">
+      <div className="flex-1 flex flex-col min-h-0 overflow-hidden pt-16 md:pt-20 pb-4 md:pb-6 px-4 sm:px-6 lg:px-8">
         <div className="flex-1 flex flex-col min-h-0 max-w-7xl mx-auto w-full">
           <div className="flex-1 flex flex-col min-h-0 rounded-2xl border border-white/10 bg-nearBlack/50 overflow-hidden">
             <BookingFlow
